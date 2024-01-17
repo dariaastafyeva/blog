9d7ed92fe393a74b30199e9f921fa1fdeaa8cf0c
Daria Astafyeva <mermalya17@gmail.com>
2024-01-17 15:12:41 +0300

    added api controllers, routes, and db. realized register and login functional

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/user", userRoutes);
+
+app.listen(8800, ()=>{
+    console.log("Connected!");
+});
