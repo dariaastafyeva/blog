7bade0ac8d0d3ddf580d096d84a829238417cedc
Daria Astafyeva <mermalya17@gmail.com>
2024-01-30 15:58:18 +0300

    show posts by category (main page)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,25 +12,27 @@ const Navbar = () => {
     <div className='navbar'>
       <div className="container">
         <div className="logo">
-          <img src={Logo} alt="" />
+          <Link to="/">
+            <img src={Logo} alt="" />
+          </Link>
         </div>
         <div className="links">
-          <Link className='link' to='/?art'>
+          <Link className='link' to='/?cat=art'>
             <h6>ART</h6>
           </Link>
-          <Link className='link' to='/?art'>
+          <Link className='link' to='/?cat=sience'>
             <h6>SIENCE</h6>
           </Link>
-          <Link className='link' to='/?art'>
+          <Link className='link' to='/?cat=tehnology'>
             <h6>TEHNOLOGY</h6>
           </Link>
-          <Link className='link' to='/?art'>
+          <Link className='link' to='/?cat=cinema'>
             <h6>CINEMA</h6>
           </Link>
-          <Link className='link' to='/?art'>
+          <Link className='link' to='/?cat=design'>
             <h6>DESIGN</h6>
           </Link>
-          <Link className='link' to='/?art'>
+          <Link className='link' to='/?cat=food'>
             <h6>FOOD</h6>
           </Link>
           <span>{currentUser?.username}</span>
